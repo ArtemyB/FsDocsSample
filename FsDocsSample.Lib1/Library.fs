d8582d8b70264bdae06a916877342086cd742a12
Artemy <artemy.bordushko@hotmail.com>
2021-06-06 15:28:54 +0300

    Add project files.

---
+++ FsDocsSample.Lib1/Library.fs
@@ -0,0 +1,18 @@
+﻿namespace FsDocsSample.Lib1
+
+/// <summary>
+/// Interface 1
+/// </summary>
+/// <namespacedoc>
+/// <summary>
+/// Main namespace of Lib1
+/// </summary>
+/// </namespacedoc>
+type IMyInterface1 =
+    abstract member Prop1 : string
+    abstract member Method1 : int -> string
+
+
+/// My exception type
+type MyException(msg: string) =
+    inherit System.Exception(msg)
---
+++ FsDocsSample.Lib2/Library.fs
@@ -0,0 +1,44 @@
+﻿namespace FsDocsSample.Lib2
+
+
+
+/// <summary>
+/// Module #2
+/// </summary>
+/// <namespacedoc>
+/// <summary>
+/// Main namespace of Lib2
+/// </summary>
+/// </namespacedoc>
+module MyModule2 =
+
+    open FsDocsSample.Lib1
+
+    /// Greetings function
+    let hello name =
+        printfn "Hello %s" name
+
+
+    /// Function that creates an instance of <see cref="T:FsDocsSample.Lib1.IMyInterface1"/>
+    let createObjOfInterface1 (prop1: string) (fun1: int -> string) =
+        { new IMyInterface1 with
+              member this.Method1(arg1: int): string =
+                  fun1 arg1
+
+              member this.Prop1: string = prop1 }
+
+
+    /// <summary>
+    /// Function that always throws an exception
+    /// </summary>
+    /// <exception cref="T:FsDocsSample.Lib1.MyException">
+    /// Inevitable exception
+    /// </exception>
+    let alwaysFail () =
+        raise <| MyException("Inevitable error")
+
+
+/// My interface #2
+type IMyInterface2 =
+    /// Prop2
+    abstract member Prop2 : System.DateTime
